9628e3241c88ae5ec14e79c3afcbfc602644c988
DANILO BRAYANN <116122936+danilobrayann@users.noreply.github.com>
2023-10-11 11:27:02 -0300

    feat: Styled Components - Estilos Globais

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import{ BrowserRouter }from "react-router-dom"
 import Router from './routes/routes'
-
+import GblobalStyle  from '../src/styles/globalStyle'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <BrowserRouter>
    <Router/>
    </BrowserRouter>
+   <GblobalStyle />
   </React.StrictMode>,
 )
