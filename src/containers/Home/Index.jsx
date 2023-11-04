9227a49c42e737d895def683a73eb46d6e2fb6b1
DANILO BRAYANN <116122936+danilobrayann@users.noreply.github.com>
2023-11-04 08:13:40 -0300

    feat: Estilização do Poster

--- src/containers/Home/Index.jsx
+++ src/containers/Home/Index.jsx
@@ -1,36 +1,43 @@
 import { useState, useEffect } from 'react'
 import api from '../../services/api'
-import { Background } from './styles'
+import { Background, Infor, Poster, Container } from './styles'
 
-export default function Home () {
+export default function Home() {
 
-const [Movie, SetMovie] = useState()
+    const [Movie, SetMovie] = useState()
 
-useEffect(() => {
- 
-    async function getMovies() {
-    
-        const { data: {results} } = await api.get('/movie/popular')
+    useEffect(() => {
 
-        SetMovie(results[1])
-    }
-    
-    getMovies()
- 
-}, [ ])
+        async function getMovies() {
 
+            const { data: { results } } = await api.get('/movie/popular')
 
- 
+            SetMovie(results[1])
+        }
+
+        getMovies()
+
+    }, [])
 
 
-    return(
+
+
+
+    return (
         <>
-        {Movie && (
-        <Background  img={`https://image.tmdb.org/t/p/original${Movie.backdrop_path}`}>
-            <h1>{Movie.title}</h1>
-            <p>{Movie.overview}</p>
-        </Background >
-        )}
+            {Movie && (
+                <Background img={`https://image.tmdb.org/t/p/original${Movie.backdrop_path}`}>
+                    <Container>
+                        <Infor>
+                            <h1>{Movie.title}</h1>
+                            <p>{Movie.overview}</p>
+                        </Infor>
+                        <Poster>
+                            <img src={`https://image.tmdb.org/t/p/original${Movie.poster_path}`} alt="Capa-do-FilmeS" />
+                        </Poster>
+                    </Container>
+                </Background >
+            )}
         </>
     )
 }
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -20,3 +20,43 @@ background-color:rgba(0,0,0,0.5);
 
 }
 `
+
+export const Container = styled.div`
+display:flex;
+justify-content:space-around;
+align-items:center;
+height:100%;
+max-width:1500px;
+`
+
+export const Infor = styled.div`
+
+z-index:2;
+padding:20px;
+width:50%;
+
+h1{
+    font-size: 5rem;
+    font-weight:700;
+    color: #ffffff;
+}
+
+p{
+    font-size:20px;
+    font-weight:500;
+    color: #ffffffff;
+    margin-top:30px;
+    margin-bottom:20px;
+}
+`
+
+export const Poster = styled.div`
+ z-index:3;
+img{
+    width:400px;
+    border-radius:50px;
+   
+}
+
+`
+
