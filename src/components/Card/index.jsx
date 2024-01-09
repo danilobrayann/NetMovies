8314d7d6dd03358758caab7199476a4c0f5fea3e
Danilo Souza <danilobrayann@gmail.com>
2024-01-08 21:06:21 -0300

    feat: novas informações da api

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,7 +5,7 @@ export default function Card({item }) {
     return (
         <Container>
             <img src={getImages(item.poster_path || item.profile_path || '')} alt="" />
-            <h3>{item.title}</h3>
+            <h3>{item.title || item.name}</h3>
 
         </Container>
     )
